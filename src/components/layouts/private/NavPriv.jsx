f00de50918851266b3bbc304c6d7f387b6bb9524
Kerly Sarrias <kerlysarrias011@gmail.com>
2024-11-04 21:47:40 -0500

    routes nav private and component for error 404

--- src/components/layouts/private/NavPriv.jsx
+++ src/components/layouts/private/NavPriv.jsx
@@ -7,21 +7,21 @@ export const NavPriv = () => {
 
       <ul className="container-lists__menu-list">
         <li className="menu-list__item">
-          <NavLink to='/feed' className="menu-list__link">
+          <NavLink to='feed' className="menu-list__link">
             <i className="fa-solid fa-house"></i>
             <span className="menu-list__title">Inicio</span>
           </NavLink>
         </li>
 
         <li className="menu-list__item">
-          <NavLink to='/feed' className="menu-list__link">
+          <NavLink to='feed' className="menu-list__link">
             <i className="fa-solid fa-list"></i>
             <span className="menu-list__title">Timeline</span>
           </NavLink>
         </li>
 
         <li className="menu-list__item">
-          <NavLink to='/people' className="menu-list__link">
+          <NavLink to='people' className="menu-list__link">
             <i className="fa-solid fa-users"></i>
             <span className="menu-list__title">Gente</span>
           </NavLink>
@@ -40,10 +40,10 @@ export const NavPriv = () => {
           </a>
         </li>
         <li className="list-end__item">
-          <a href='#'className="list-end__link">
+          <NavLink to='settings'className="list-end__link">
           <i className="fa-solid fa-gear"></i>
             <span className="list-end__name">Ajustes</span>
-          </a>
+          </NavLink>
         </li>
         <li className="list-end__item">
           <a href='#' className="list-end__link">
--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -4,29 +4,35 @@ import { PrivateLayout } from "../components/layouts/private/PrivateLayout"
 import { Login } from "../components/user/Login"
 import { Register } from "../components/user/Register"
 import Feed from "../components/publication/feed"
+import Config from "../components/user/Config"
+import People from "../components/user/People"
+import { Error404 } from "../components/layouts/Error404"
 
 export const Routing = () => {
   return (
     <BrowserRouter>
       <Routes>
         {/* Cargamos los componentes de la ruta pública */}
-        <Route path="/" element={<PublicLayout/>}>
-        <Route index element={<Login/>} />
-        <Route path="login" element={<Login />}/>
-        <Route path="register" element={<Register/>}/>
-          
+        <Route path="/" element={<PublicLayout />}>
+          <Route index element={<Login />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+
 
 
 
         </Route>
 
         {/* Cargamos los componentes de la ruta privada */}
-        <Route path="/rsocial" element={<PrivateLayout/>}>
-        <Route index element={<Feed/>} />
-        <Route path="feed" element={<Feed/>}/>
-        <Route path="people" element={<Feed/>}/>
-
+        <Route path="/rsocial" element={<PrivateLayout />}>
+          <Route index element={<Feed />} />
+          <Route path="feed" element={<Feed />} />
+          <Route path="people" element={<People />} />
+          <Route path="settings" element={<Config />} />
         </Route>
+
+        <Route path="*" element={<Error404 />} />
+
       </Routes>
     </BrowserRouter>
   )
