a7e155b9882adcecde3ac222a6200a82e34fceb9
Kerly Sarrias <kerlysarrias011@gmail.com>
2024-11-06 15:46:30 -0500

    components publications

--- src/components/publication/Feed.jsx
+++ src/components/publication/Feed.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
-import PublicationList from './PublicationList.jsx'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Global } from '../../helpers/Global';
+import { PublicationList } from '../publication/PublicationList';
 
-const Feed = () => {
+export const Feed = () => {
+
+    const [publications, setPublications] = useState([]);
+    const [page, setPage] = useState(1);
+    const [more, setMore] = useState(true);
+
+    useEffect(() => {
+        getPublications(1, false);
+    }, []);
+
+    const getPublications = async (nextPage = 1, showNews = false) => {
+
+        if(showNews){
+            setPublications([]);
+            setPage(1);
+            nextPage = 1;
+        }
+
+        const request = await fetch(Global.url + "publication/feed/" + nextPage, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": localStorage.getItem("token")
+            }
+        });
+
+        const data = await request.json();
+
+        if (data.status == "success") {
+
+            let newPublications = data.publications;
+
+            if (!showNews && publications.length >= 1) {
+                newPublications = [...publications, ...data.publications];
+            }
+
+            setPublications(newPublications);
+
+            if (!showNews && publications.length >= (data.total - data.publications.length)) {
+                setMore(false);
+            }
+
+            if(data.pages <= 1){
+                setMore(false);
+            }
+        }
+    }
+
     return (
         <>
             <header className="content__header">
@@ -10,17 +59,15 @@ const Feed = () => {
             </header>
 
             <PublicationList
-                publications={''}
-                getPublications={''}
-                page={''}
-                setPage={''}
-                more={''}
-                setMore={''}
-                isProfile={''}
+                publications={publications}
+                getPublications={getPublications}
+                page={page}
+                setPage={setPage}
+                more={more}
+                setMore={setMore}
+                isProfile={false} // No es un perfil, es el feed
             />
             <br />
         </>
     )
 }
-
-export default Feed
